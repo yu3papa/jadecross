585f0d5a14f94692126bbf286bd990a1a1355cdd
Administrator <Administrator@JHY>
2019-10-14 16:31:40 +0900

    프로젝트 공유

---
+++ com.jadecross.edu.jni.java/src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+	private native void print();
+
+	static {
+		System.loadLibrary("HelloWorld");
+	}
+
+	public static void main(String[] args) {
+		new HelloWorld().print();
+	}
+}
